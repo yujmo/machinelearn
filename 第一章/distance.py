2ced414c3b7ee73c0ddef0a19472ae3141784ae6
yujmo <yujmo94@gmail.com>
2017-10-21 19:55:10 +0800

    modify some files

--- 第一章/distance.py
+++ 第一章/distance.py
@@ -25,9 +25,23 @@ vector1 = mat([4,5,6])
 vector2 = mat([4,5,6])
 print((dot(vector1,vector2.T))/(linalg.norm(vector1)*linalg.norm(vector2)))
 
+#相关系数
+featuremat = mat([[88.5,96.8,104.1,111.3,117.7,124.0,130.0,135.4,140.2,145.3,151.9,159.5,165.9,169.8,171.6,172.3,172.7],
+    [12.54,14.65,16.64,18.98,21.26,24.06,27.33,30.46,33.74,37.69,42.49,48.08,53.37,57.08,59.35,60.68,61.40]])
+print(featuremat)
 
+mv1 = mean(featuremat[0])
+mv2 = mean(featuremat[1])
+print(mv1,mv2) #打印均值
 
+dv1 = std(featuremat[0])
+dv2 = std(featuremat[1])
+print(dv1,dv2) #两列标准差
 
+corref = mean(multiply(featuremat[0]-mv1,featuremat[1]-mv2)/(dv1*dv2))
+print(corref)
+
+print(corrcoef(featuremat))
 
 
 

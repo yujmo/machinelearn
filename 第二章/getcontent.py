53cbcf36a34254c6b8396ca00311d12ccbbff14b
yujmo <yujmo94@gmail.com>
2017-10-22 16:11:07 +0800

    add some files

--- 第二章/getcontent.py
+++ 第二章/getcontent.py
@@ -1,5 +1,8 @@
-import chardet
-import urllib
-data1 = urllib.urlopen('http://www.163.com').read()
-chardit1 = chardet.detect(data1)
-print(chardit1)
+import requests
+from lxml import etree,html
+from bs4 import BeautifulSoup
+webdata = requests.get('http://www.163.com')
+webpage = BeautifulSoup(webdata.text,"lxml")
+page = html.document_fromstring(str(webpage))
+text = page.text_content()
+print(text) 

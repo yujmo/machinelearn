977fdf6d24de1ecb0b766ad2f6d1a5a9ef8c7656
yujmo <yujmo94@gmail.com>
2017-10-26 10:22:45 +0800

    add some files

---
+++ 第三章/id3tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from numpy import *
+import math
+import copy
+import pickle
+class ID3DTree(object):
+    def __init__(self):
+        self.tree = {}
+        self.dataSet = []
+        self.labels = []
+    def loadDataSet(self,path,labels):
+

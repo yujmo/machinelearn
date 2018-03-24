05e97cfe34ceaf79148a76776080b5efa9d8b041
yujmo <yujmo@users.noreply.github.com>
2018-03-24 10:22:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -6,42 +6,26 @@ import cv2
 import numpy as np 
 
 mnist = input_data.read_data_sets(".",one_hot = True)  
-
-
 sess = tf.InteractiveSession()  
-
-
 x = tf.placeholder("float", shape=[None, 784], name="Mul")  
 y_ = tf.placeholder("float",shape=[None, 10],  name="y_")  
-
 W = tf.Variable(tf.zeros([784,10]),name='x')  
 b = tf.Variable(tf.zeros([10]),'y_')  
-
 
 def weight_variable(shape):  
   initial = tf.truncated_normal(shape, stddev=0.1)  
   return tf.Variable(initial)  
 
-
-
 def bias_variable(shape):  
   initial = tf.constant(0.1, shape=shape)  
   return tf.Variable(initial)  
 
-
-
-
 def conv2d(x, W):  
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')  
-
-
 
 def max_pool_2x2(x):  
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],  
                         strides=[1, 2, 2, 1], padding='SAME')  
-
-
-
 
 W_conv1 = weight_variable([5, 5, 1, 32])  
 b_conv1 = bias_variable([32])  
@@ -50,9 +34,6 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)  
 W_conv2 = weight_variable([5, 5, 32, 64])  
 b_conv2 = bias_variable([64]) 
-
-
-
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)  
 h_pool2 = max_pool_2x2(h_conv2)  
@@ -66,39 +47,18 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 keep_prob = tf.placeholder("float",name='rob')  
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)  
 
-
-
-
-
 y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2,name='res')  
-
-
 y_conv2=tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2,name="final_result")  
 
-
-
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))  
-
-
-
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)  
-
-
-
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))  
-
-
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))  
-
-
 sess.run(tf.global_variables_initializer())  
-
 
 tf.add_to_collection('res', y_conv)  
 tf.add_to_collection('output', y_conv2)  
 tf.add_to_collection('x', x)  
-
-
 
 for i in range(200):  
   batch = mnist.train.next_batch(50)  
@@ -107,20 +67,12 @@ for i in range(200):
         x:batch[0], y_: batch[1], keep_prob: 1.0})  
     print "step %d, training accuracy %g"%(i, train_accuracy)  
 
-
-
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})  
-
-
-
-
 graph_def = tf.get_default_graph().as_graph_def()   
 
 
 output_graph_def = tf.graph_util.convert_variables_to_constants(sess,    
                 graph_def, ['final_result'])    
-
-
 
 with tf.gfile.GFile("grf.pb", 'wb') as f:    
         f.write(output_graph_def.SerializeToString())  
